a19233d0736d2504ed7481c01d67741dfe9569c7
Birght <shi_19890905@163.com>
2018-11-30 11:35:27 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+      <h1>for  love </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  background-color: skyblue;
+  height: 100vh;
+}
+</style>
